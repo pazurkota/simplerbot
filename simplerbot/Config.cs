b408cf0e651d47931265263009502aef818bfc67
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-03-15 13:56:16 +0100

    Basic bot configuration

---
+++ simplerbot/Config.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace simplerbot;
+
+public class Config
+{
+    [JsonProperty("token")] public string Token { get; set; }
+    [JsonProperty("prefix")] public string Prefix { get; set; }
+
+    public static Config LoadConfig()
+    {
+        var path = File.ReadAllText("config.json");
+        var data = JsonConvert.DeserializeObject<Config>(path);
+
+        return data;
+    }
+}
--- simplerbot/Program.cs
+++ simplerbot/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
 
-Console.WriteLine("Hello, World!");
+namespace simplerbot;
+
+public class Program
+{
+    static async Task Main()
+    {
+        var json = Config.LoadConfig();
+
+        var config = new DiscordConfiguration
+        {
+            Token = json.Token,
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.All,
+            AutoReconnect = true
+        };
+
+        var client = new DiscordClient(config);
+
+        var commands = new CommandsNextConfiguration
+        {
+            StringPrefixes = [json.Prefix]
+        };
+        
+        var commandsConfig = client.UseCommandsNext(commands);
+        var activity = new DiscordActivity($"{json.Prefix}help to get started", ActivityType.Watching);
+        
+        await client.ConnectAsync(activity);
+        await Task.Delay(-1);
+    }
+}
